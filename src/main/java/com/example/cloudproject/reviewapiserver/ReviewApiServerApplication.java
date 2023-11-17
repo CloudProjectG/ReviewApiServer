6632af0f3159359fbb7650590c4b6b4b239a748e
Untaini <untaini@naver.com>
2023-11-17 23:18:05 +0900

    initial commit

---
+++ src/main/java/com/example/cloudproject/reviewapiserver/ReviewApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudproject.reviewapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewApiServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloudproject/reviewapiserver/ReviewApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudproject.reviewapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
