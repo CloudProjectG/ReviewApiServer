979a012b585fd74c0f61ebee134702297488d81e
Untaini <untaini@naver.com>
2023-11-19 03:20:10 +0900

    feat(service): getStoreReviews() 추가
    한 페이의 행 길이와 페이지 넘버를 받아
    해당 페이지의 리뷰 목록을 반환해 주는
    getStoreReviews() 함수를 추가함

--- src/main/java/com/example/cloudproject/reviewapiserver/service/ReviewService.java
+++ src/main/java/com/example/cloudproject/reviewapiserver/service/ReviewService.java
@@ -1,6 +1,14 @@
 package com.example.cloudproject.reviewapiserver.service;
 
+import com.example.cloudproject.reviewapiserver.dto.StoreReviewDTO;
+import com.example.cloudproject.reviewapiserver.dto.StoreReviewRequestDTO;
+import com.example.cloudproject.reviewapiserver.dto.StoreReviewResponseDTO;
 import com.example.cloudproject.reviewapiserver.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,8 +16,27 @@ public class ReviewService {
 
     private final ReviewRepository reviewRepository;
 
+    @Autowired
     public ReviewService(ReviewRepository reviewRepository) {
         this.reviewRepository = reviewRepository;
+    }
+
+    public StoreReviewResponseDTO getStoreReviews(StoreReviewRequestDTO storeReviewRequestDTO) {
+        Pageable pageable = PageRequest.of(
+                storeReviewRequestDTO.getPage(),
+                storeReviewRequestDTO.getRow(),
+                Sort.by(Sort.Direction.DESC, "createdAt")
+        );
+
+        Page<StoreReviewDTO> page = reviewRepository
+                .findAllByStoreIdAndIsHiddenFalse(storeReviewRequestDTO.getStoreId(), pageable)
+                .map(StoreReviewDTO::from);
+
+        return StoreReviewResponseDTO.builder()
+                .page(storeReviewRequestDTO.getPage())
+                .row(storeReviewRequestDTO.getRow())
+                .reviews(page.getContent())
+                .build();
     }
 
 }
