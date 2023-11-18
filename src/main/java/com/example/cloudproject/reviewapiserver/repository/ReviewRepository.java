7914288d5d37f2c4a54efc9bd4b33fca309d4b76
Untaini <untaini@naver.com>
2023-11-19 02:32:59 +0900

    feat(repository): findAllByStoreIdAndIsHiddenFalse() 추가
    target: ReviewRepository
      가게 id와 페이지 기준을 받아
      가게 id가 일치하면서 숨겨지지 않은 리뷰들을 DB에서 검색하고
      결과를 Page<Review> 타입으로 반환해 주는
      findAllByStoreIdAndIsHiddenFalse() 함수를 추가함

--- src/main/java/com/example/cloudproject/reviewapiserver/repository/ReviewRepository.java
+++ src/main/java/com/example/cloudproject/reviewapiserver/repository/ReviewRepository.java
@@ -1,8 +1,12 @@
 package com.example.cloudproject.reviewapiserver.repository;
 
 import com.example.cloudproject.reviewapiserver.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Review.ReviewPK> {
+
+    Page<Review> findAllByStoreIdAndIsHiddenFalse(Long storeId, Pageable pageable);
 
 }
