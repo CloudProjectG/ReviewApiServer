efc34a8fcc396ea23a51e63e441f7e5cff53d9a1
Untaini <untaini@naver.com>
2023-11-18 19:06:42 +0900

    feat(entity): 리뷰 엔티티 생성
    리뷰 엔티티 컬럼
    - storeId: Long //가게 table의 Id
    - userId: Long //유저 table의 Id
    - menu: String //리뷰할 메뉴
    - grade: Byte //리뷰 평점
    - comment: String //한줄평
    - hashtag1Id: Short //해시태그 table의 id
    - hashtag2Id: Short //해시태그 table의 id
    - hashtag3Id: Short //해시태그 table의 id
    - imageUuid: UUID //s3에 저장할 이미지의 UUID
    - isHidden: Boolean //리뷰의 숨김 여부
    - isAnonymous: Boolean //리뷰의 익명 여부
    - createdAt: Timestamp //리뷰 생성 시각 - 추후 페이징 처리에 사용
    
    storeId와 userId는
    직렬화 가능한 ReviewPK 클래스에 넣어
    JPA 복합키 구성이 가능하도록 만듦

---
+++ src/main/java/com/example/cloudproject/reviewapiserver/entity/Review.java
@@ -0,0 +1,67 @@
+package com.example.cloudproject.reviewapiserver.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Reviews")
+@IdClass(Review.ReviewPK.class)
+@EqualsAndHashCode(exclude = {"storeId", "userId"})
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Review {
+
+    @Id
+    @Column(name = "store_id")
+    private Long storeId;
+
+    @Id
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(nullable = false)
+    private String menu;
+
+    @Column(nullable = false)
+    private Byte grade;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @Column(name = "hasgtag1_id", nullable = false)
+    private Short hashtag1Id;
+
+    @Column(name = "hasgtag2_id", nullable = false)
+    private Short hashtag2Id;
+
+    @Column(name = "hasgtag3_id", nullable = false)
+    private Short hashtag3Id;
+
+    @Column(name = "image_uuid", unique = true)
+    private UUID imageUuid;
+
+    @Column(name = "hidden", nullable = false)
+    private Boolean isHidden;
+
+    @Column(name = "anonymous", nullable = false)
+    private Boolean isAnonymous;
+
+    @Column(name = "created_at", nullable = false)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Data
+    @NoArgsConstructor
+    public class ReviewPK implements Serializable {
+        private Long storeId;
+        private Long userId;
+    }
+
+}
