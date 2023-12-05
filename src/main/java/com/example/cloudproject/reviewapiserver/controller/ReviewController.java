467196ad3f21a8805cbfb228fdd50cd8b298487c
Untaini <untaini@naver.com>
2023-12-06 03:05:59 +0900

    feat(controller): POST /review/{storeId} 추가
    리뷰를 등록할 때 사용하는 API 추가
     url: /review/{storeId}
     method: POST

--- src/main/java/com/example/cloudproject/reviewapiserver/controller/ReviewController.java
+++ src/main/java/com/example/cloudproject/reviewapiserver/controller/ReviewController.java
@@ -1,7 +1,11 @@
 package com.example.cloudproject.reviewapiserver.controller;
 
+import com.example.cloudproject.reviewapiserver.dto.ReviewDTO;
 import com.example.cloudproject.reviewapiserver.dto.StoreReviewDTO;
+import com.example.cloudproject.reviewapiserver.exception.AuthException;
+import com.example.cloudproject.reviewapiserver.exception.type.AuthExceptionType;
 import com.example.cloudproject.reviewapiserver.service.ReviewService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +22,7 @@ public class ReviewController {
     }
 
     @GetMapping("/{storeId}")
-    public ResponseEntity<StoreReviewDTO.Response> getStoreReviews(@PathVariable Long storeId,
+    public ResponseEntity<StoreReviewDTO.Response> getStoreReviews(@PathVariable("storeId") Long storeId,
                                                                   @ModelAttribute StoreReviewDTO.Request requestDTO) {
 
         requestDTO.setStoreId(storeId);
@@ -26,6 +30,22 @@ public class ReviewController {
         StoreReviewDTO.Response responseDTO = reviewService.getStoreReviews(requestDTO);
 
         return ResponseEntity.ok(responseDTO);
+    }
+
+    @PostMapping("{storeId}")
+    public ResponseEntity<ReviewDTO.CreateResponse> postReview(@PathVariable("storeId") Long storeId,
+                                                               @ModelAttribute ReviewDTO.CreateRequest requestDTO,
+                                                               HttpServletRequest servletRequest) {
+
+        Long userId = (Long) servletRequest.getAttribute("userId");
+        if (userId == -1) {
+            throw new AuthException(AuthExceptionType.UNAUTHORIZED_TOKEN);
+        }
+
+        requestDTO.setStoreId(storeId);
+        requestDTO.setUserId(userId);
+
+        return ResponseEntity.ok(reviewService.createReview(requestDTO));
     }
 
 }
