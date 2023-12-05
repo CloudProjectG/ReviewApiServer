8c233a62c4af9e6a1b825b0c02e9072913a1fbb8
Untaini <untaini@naver.com>
2023-12-06 03:00:28 +0900

    refactor(exception): 리뷰 저장 시 발생하는 예외를 조금 더 포괄적으로 변경

--- src/main/java/com/example/cloudproject/reviewapiserver/exception/type/ReviewExceptionType.java
+++ src/main/java/com/example/cloudproject/reviewapiserver/exception/type/ReviewExceptionType.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ReviewExceptionType implements BaseExceptionType {
-    STORE_NOT_FOUND("R001", HttpStatus.NOT_FOUND, "리뷰를 저장할 가게를 찾을 수 없습니다."),
+    REVIEW_SAVE_FAILURE("R001", HttpStatus.NOT_FOUND, "리뷰 저장을 실패했습니다."),
     ONLY_ONE_REVIEW_PER_STORE("R002", HttpStatus.BAD_REQUEST, "가게 당 하나의 리뷰만 작성할 수 있습니다.")
     ;
 
