eb418705bfba457e47c19de24c571f6c31d60cfd
Untaini <untaini@naver.com>
2023-12-11 03:33:34 +0900

    feat(repository): store id를 기준으로 모든 리뷰를 가져오는 함수 추가

--- src/main/java/com/example/cloudproject/reviewapiserver/repository/ReviewRepository.java
+++ src/main/java/com/example/cloudproject/reviewapiserver/repository/ReviewRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Review.ReviewPK> {
 
     Page<Review> findAllByStoreIdAndIsHiddenFalse(Long storeId, Pageable pageable);
     Page<Review> findAllByUserId(Long userId, Pageable pageable);
+    List<Review> findAllByStoreId(Long storeId);
 
 }
